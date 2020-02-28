04d4b2949c3185b18ccb64a117233beb18fe3db3
Hoi <52797529+hkcheung1217@users.noreply.github.com>
2020-02-28 00:51:09 -0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
+import SignIn from "./components/sign-in/sign-in.component";
+import SignUp from "./components/sign-up/sign-up.component";
+
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <Switch>
+      <Route exact path="/react-node-signup-signin" component={SignIn} />
+      <Route exact path="/react-node-signup-signin/signup" component={SignUp} />
+    </Switch>
+  </div>
+);
+
+export default App;
